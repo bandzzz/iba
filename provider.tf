878edcfe29a5b8b7c470a55ea036655b5325de35
bandzzz <30724404+bandzzz@users.noreply.github.com>
2025-01-28 17:58:48 +0300

    provider.tf

---
+++ provider.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "us-east-1a"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "bandzzz-terraform-state-bucket"
+    key            = "terraform/state.tfstate"
+    region         = "us-east-1a"
+    dynamodb_table = "bandzzz-dynamodb-lock-table"
+    encrypt        = true
+  }
+}
+
+resource "aws_dynamodb_table" "bandzzz-dynamodb-lock-table" {
+  name         = "bandzzz-dynamodb-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
