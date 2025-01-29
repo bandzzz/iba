1c93eaffb1cc35083ab4247e2ec95a87fb84c0be
bandzzz <30724404+bandzzz@users.noreply.github.com>
2025-01-29 23:02:44 +0300

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "bandzzz-terraform-state-bucket"
-    key            = "terraform/state.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "bandzzz-dynamodb-lock-table"
-    encrypt        = true
-  }
-}
