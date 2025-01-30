a7b5694392ae909c16cdc6363c767865bbe50259
bandzzz <30724404+bandzzz@users.noreply.github.com>
2025-01-30 17:56:25 +0300

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,3 +1,4 @@
+# Создание роли для EKS кластера
 resource "aws_iam_role" "bandzzz_eks_cluster_role_202501" {
   name = "bandzzz_eks_cluster_role_202501"
 
@@ -20,6 +21,7 @@ resource "aws_iam_role_policy_attachment" "bandzzz_eks_cluster_policy_202501" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
+# Создание роли для узлов EKS
 resource "aws_iam_role" "bandzzz_eks_node_role_202501" {
   name = "bandzzz_eks_node_role_202501"
 
@@ -37,16 +39,19 @@ resource "aws_iam_role" "bandzzz_eks_node_role_202501" {
   })
 }
 
+# Прикрепление политики к роли EKS кластера предоставляет разрешения для узлов управления EKS
 resource "aws_iam_role_policy_attachment" "bandzzz_eks_worker_node_policy_202501" {
   role       = aws_iam_role.bandzzz_eks_node_role_202501.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
+# Прикрепление политик к роли узлов EKS предоставляет права для работы с сетевыми интерфейсами в EKS
 resource "aws_iam_role_policy_attachment" "bandzzz_eks_cni_policy_202501" {
   role       = aws_iam_role.bandzzz_eks_node_role_202501.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
+# Предоставляет доступ к образам контейнеров в ECR для чтения
 resource "aws_iam_role_policy_attachment" "bandzzz_ec2_container_registry_readonly_202501" {
   role       = aws_iam_role.bandzzz_eks_node_role_202501.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
