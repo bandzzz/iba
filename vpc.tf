aad95cf975afb23bfee5c525598347f7d59347ef
bandzzz <30724404+bandzzz@users.noreply.github.com>
2025-01-30 12:09:43 +0300

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -10,6 +10,7 @@ resource "aws_vpc" "bandzzz_vpc" {
 resource "aws_subnet" "subnet_a" { 
   vpc_id            = aws_vpc.bandzzz_vpc.id 
   cidr_block        = "10.0.1.0/24" 
+  map_public_ip_on_launch = true
   availability_zone = "us-east-1a" 
   tags = { 
     Name = "bandzzz-subnet-a" 
@@ -18,7 +19,8 @@ resource "aws_subnet" "subnet_a" {
 
 resource "aws_subnet" "subnet_b" { 
   vpc_id            = aws_vpc.bandzzz_vpc.id 
-  cidr_block        = "10.0.2.0/24" 
+  cidr_block        = "10.0.2.0/24"
+  map_public_ip_on_launch = true
   availability_zone = "us-east-1b" 
   tags = { 
     Name = "bandzzz-subnet-b" 
