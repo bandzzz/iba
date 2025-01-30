6caa85456ebcf892303db06196723730c36b086d
bandzzz <30724404+bandzzz@users.noreply.github.com>
2025-01-30 09:59:06 +0300

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,26 +1,32 @@
-resource "aws_vpc" "bandzzz_vpc_202501" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+resource "aws_vpc" "bandzzz_vpc" { 
+  cidr_block           = "10.0.0.0/16" 
+  enable_dns_support   = true 
+  enable_dns_hostnames = true 
+  tags = { 
+    Name = "bandzzz" 
+  } 
+} 
 
-  tags = {
-    Name = "bandzzz_vpc_202501"
-  }
-}
+resource "aws_subnet" "subnet_a" { 
+  vpc_id            = aws_vpc.bandzzz_vpc.id 
+  cidr_block        = "10.0.1.0/24" 
+  availability_zone = "us-east-1a" 
+  tags = { 
+    Name = "bandzzz-subnet-a" 
+  } 
+} 
 
-resource "aws_subnet" "bandzzz_subnet_202501" {
-  count             = 2
-  vpc_id            = aws_vpc.bandzzz_vpc_202501.id
-  cidr_block        = cidrsubnet(aws_vpc.bandzzz_vpc_202501.cidr_block, 4, count.index)
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-
-  tags = {
-    Name = "bandzzz-subnet-${count.index + 1}"
-  }
+resource "aws_subnet" "subnet_b" { 
+  vpc_id            = aws_vpc.bandzzz_vpc.id 
+  cidr_block        = "10.0.2.0/24" 
+  availability_zone = "us-east-1b" 
+  tags = { 
+    Name = "bandzzz-subnet-b" 
+  } 
 }
 
 resource "aws_internet_gateway" "bandzzz_igw_202501" {
-  vpc_id = aws_vpc.bandzzz_vpc_202501.id
+  vpc_id = aws_vpc.bandzzz_vpc.id
 
   tags = {
     Name = "bandzzz_igw_202501"
@@ -28,7 +34,7 @@ resource "aws_internet_gateway" "bandzzz_igw_202501" {
 }
 
 resource "aws_route_table" "bandzzz_route_table_202501" {
-  vpc_id = aws_vpc.bandzzz_vpc_202501.id
+  vpc_id = aws_vpc.bandzzz_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -42,6 +48,6 @@ resource "aws_route_table" "bandzzz_route_table_202501" {
 
 resource "aws_route_table_association" "bandzzz_route_assoc_202501" {
   count          = 2
-  subnet_id      = aws_subnet.bandzzz_subnet_202501[count.index].id
+  subnet_id      = element([aws_subnet.subnet_a.id, aws_subnet.subnet_b.id], count.index)
   route_table_id = aws_route_table.bandzzz_route_table_202501.id
 }
