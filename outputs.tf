14e57e01a781b54840c0ddc4404aa4cfa53af3a7
bandzzz <30724404+bandzzz@users.noreply.github.com>
2025-01-30 17:57:24 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,7 +9,7 @@ output "eks_cluster_name" {
   value = aws_eks_cluster.bandzzz_eks_cluster_202501.name
 }
 
-# Вывод инфомрациии о созданом ECR репозитории
+# Вывод информациии о созданом ECR репозитории
 output "ecr_repository_url" {
   value = aws_ecr_repository.bandzzz_ecr_repo_202501.repository_url
 }
