c4c435d7a035affeff9de6ba48dd9bc0cda1b524
bandzzz <30724404+bandzzz@users.noreply.github.com>
2025-01-30 17:55:20 +0300

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,3 +1,4 @@
+# Создание кластера Amazon EKS + роли IAM для кластера
 resource "aws_eks_cluster" "bandzzz_eks_cluster_202501" {
   name     = var.cluster_name
   role_arn = aws_iam_role.bandzzz_eks_cluster_role_202501.arn
@@ -11,6 +12,7 @@ resource "aws_eks_cluster" "bandzzz_eks_cluster_202501" {
   ]
 }
 
+# Создание группы узлов для кластера EKS + роли IAM для узлов
 resource "aws_eks_node_group" "bandzzz_eks_node_group_202501" {
   cluster_name    = aws_eks_cluster.bandzzz_eks_cluster_202501.name
   node_group_name = "bandzzz_eks_node_group_202501"
@@ -25,6 +27,7 @@ resource "aws_eks_node_group" "bandzzz_eks_node_group_202501" {
 
   instance_types = [var.node_instance_type]
 
+# Определяее зависимости, что необходимые политики IAM присоединены к ролям
   depends_on = [
     aws_iam_role_policy_attachment.bandzzz_eks_worker_node_policy_202501,
     aws_iam_role_policy_attachment.bandzzz_eks_cni_policy_202501,
