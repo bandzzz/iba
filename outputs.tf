638030a0ac308445ce02bfdf9fde0f20a5a949bb
bandzzz <30724404+bandzzz@users.noreply.github.com>
2025-01-30 12:45:44 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,8 @@
 output "vpc_id" {
   value = aws_vpc.bandzzz_vpc.id
 }
-
-output "eks_cluster_name" {
-  value = aws_eks_cluster.bandzzz_eks_cluster_202501.name
-}
-
 output "ecr_repository_url" {
   value = aws_ecr_repository.bandzzz_ecr_repo_202501.repository_url
 }
+
+
